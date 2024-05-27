515a233ce5c9f301ca7cd4342f171dc05709a1b2
Mayur Shrotriya <103096253+mayur-sh@users.noreply.github.com>
2024-05-27 15:28:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,13 @@ from utils import *
 
 st.title("Get data from Database.")
 
+st.page_link("https://raw.githubusercontent.com/mayurzlr/ai_query_db/main/data/SakilaDatabaseERD.png", label="Database ERD🔗")
+
 question = st.text_input("Enter you question here.")
 
 
 if question:
-    db = sql.connect("D:/Data Science Practices/ai_db_query/data/sqlite-sakila.db/sqlite-sakila.db")
+    db = sql.connect("data/sqlite-sakila.db/sqlite-sakila.db")
     
     chat_session = model.start_chat(
     history=[]
